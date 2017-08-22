2cead2718dbf45c9e3e39034efff76a5b147b703
lapsap <hihitan@gmail.com>
2017-08-22 14:15:46 +0800

    Initial Commit

---
+++ jb_detection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jb_detection
+//
+//  Created by Chris Tan on 22/08/2017.
+//  Copyright © 2017 Chris Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ jb_detectionTests/jb_detectionTests.swift
@@ -0,0 +1,36 @@
+//
+//  jb_detectionTests.swift
+//  jb_detectionTests
+//
+//  Created by Chris Tan on 22/08/2017.
+//  Copyright © 2017 Chris Tan. All rights reserved.
+//
+
+import XCTest
+@testable import jb_detection
+
+class jb_detectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
